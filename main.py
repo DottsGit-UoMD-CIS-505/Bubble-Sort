e11a9980ede4318a164c73c10ccbf50f96397523
DottsGit <6w1598yft5kp@maskedmails.com>
2023-12-01 17:24:20 -0500

    Added more test cases

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ Author: Nicholas Butzke
 
 from bubble_sort import bubble_sort
 
-input_data = [2, 5, 1, 4, 3]
+arr_1 = [2, 5, 1, 4, 3]
+arr_2 = [3, 1, 5, 4, 2]
+arr_3 = [5, 4, 3, 2, 1]
 
-print(f"Final output: {bubble_sort(input_data)}")
+input_arrs = [arr_1, arr_2, arr_3]
+
+solution_arr = [1, 2, 3, 4, 5]
+
+for arr in input_arrs:
+    output_arr = bubble_sort(arr)
+    print(f"Final output: {output_arr}")
+    print(f"Passed: {output_arr == solution_arr}")
